70bcc087933d889e54e22059e5900bacc533f40d
anniewormus <annie@wormus.com>
2022-04-14 13:43:41 -0400

    added objective screen

---
+++ Assets/ShowObjective.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowObjective : MonoBehaviour
+{
+    public GameObject objectiveScreenUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waiter());
+    }
+
+    // Update is called once per frame
+    IEnumerator waiter()
+    {
+        // Doesn't start game
+        Time.timeScale = 0f; 
+        // Shows objective screen for 10 seconds
+        yield return new WaitForSecondsRealtime(10);
+        objectiveScreenUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
