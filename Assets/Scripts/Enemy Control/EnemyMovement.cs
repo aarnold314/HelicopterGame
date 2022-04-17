7dce0a4654ba2cc488b1612ccde4144d069c8436
Arianna Walters <29714726+AriannaWalters@users.noreply.github.com>
2022-04-17 17:40:13 -0400

    create enemy prefab

---
+++ Assets/Scripts/Enemy Control/EnemyMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(10, 10);
+    public Vector2 direction = new Vector2(-1, 0);
+
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+
+    void Update()
+    {
+        movement = new Vector2(
+          speed.x * direction.x,
+          speed.y * direction.y);
+    }
+
+    void FixedUpdate()
+    {
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        // Apply movement to the rigidbody
+        rigidbodyComponent.velocity = movement;
+    }
+}
