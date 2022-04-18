f9ecb848789b7fd91e142e65b571b8574e39770d
anniewormus <annie@wormus.com>
2022-04-18 12:01:39 -0400

    diasbled player movement when it collides with enemy

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    // creates reference to player movement script 
+    public PlayerMovement movement;
+
     void OnCollisionEnter2D (Collision2D collisionInfo)
     {
-        Debug.Log(collisionInfo.collider.name);
+        if (collisionInfo.collider.tag == "Enemy")
+        {
+            // currently disables player movement when it collides with an enemy
+            // change to an explosion graphic
+           // movement.enabled = false;
+        }
     }
 }
