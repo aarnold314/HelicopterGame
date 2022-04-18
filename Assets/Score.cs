483a27af29b6a3db174cae4964ea3d9537291c2a
anniewormus <annie@wormus.com>
2022-04-18 14:28:57 -0400

    working score tag based on marker

---
+++ Assets/Score.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    // reference to player
+    public Transform player;
+    // reference to text
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gets the units traveled in the x direction by player
+        scoreText.text = player.position.x.ToString("0");
+
+
+    }
+}
