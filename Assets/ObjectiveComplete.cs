d1344dd88629ccd7687da27b2709ea6b18558c85
anniewormus <annie@wormus.com>
2022-04-19 13:04:54 -0400

    linked checkpoint to store

--- Assets/ObjectiveComplete.cs
+++ Assets/ObjectiveComplete.cs
@@ -1,21 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObjectiveComplete : MonoBehaviour
 {
+    // public GameObject pauseScreen;
+    public GameObject showScreen;
+    
+    [SerializeField] float waitTime = 5f;
+
     // Player reaches end zone
     void OnCollisionEnter2D (Collision2D collisionInfo)
     {
         if (collisionInfo.collider.tag == "Player")
         {
-           Debug.Log("Touchdown");
-        //    StartCoroutine(EnableBox(1.0f));
+            Debug.Log("Touchdown");
+            Freeze();
         }
     }
 
-    // IEnumerator EnableBox(float waitTime)
-    // {
-    //     yield return new WaitForSeconds(waitTime);
-    // }
+    void Freeze ()
+    {
+        // freezes game
+        Time.timeScale = 0f;
+        showScreen.SetActive(true);
+        StartCoroutine(EnableBox(waitTime));
+    }
+
+    IEnumerator EnableBox (float waitTime)
+    {
+        // Shows objective screen for 10 seconds
+        yield return new WaitForSecondsRealtime(waitTime);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Shop");
+        // Time.timeScale = 1f;
+
+        // yield return new WaitForSeconds(waitTime);
+    }
 }
