335004f7b78466d7a7934199df1ba6f4eaf87507
cjlysek <cjlysek@knights.ucf.edu>
2022-04-20 18:33:17 -0400

    Extra Tank Enemy + Movement
    Added in an extra tank enemy since that was something we thought would be nice to have another enemy. Also adjusted the script to make it move on the ground as well. (Not sure how I would go about animating the tank moving or rolling perhaps)

---
+++ Assets/Scripts/Enemy Control/TankMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+	[SerializeField] private float frequency = 5f;
+	[SerializeField] private float magnitude = 5f;
+
+	public Vector2 speed = new Vector2(10, 0);
+	public bool hit = false;
+
+	private Rigidbody2D rigidbodyComponent;
+	private const string cameraTag = "MainCamera";
+	private const string playerTag = "Player";
+
+	private void Awake()
+	{
+		rigidbodyComponent = GetComponent<Rigidbody2D>();
+		rigidbodyComponent.freezeRotation = true;
+	}
+
+	private void FixedUpdate()
+	{
+		// Enemy move path
+		var direction = new Vector2(-1, Mathf.Sin(Time.time * frequency) * magnitude);
+		Vector2 movement;
+		// Removes enemy movement after collision
+		if (hit)
+		{
+			movement = new Vector2(0, 0);
+			Destroy(this);
+		}
+		else
+		{
+			movement = new Vector2(
+				speed.x * direction.x,
+				speed.y * direction.y
+			);
+		}
+
+		// Apply movement to the rigidbody
+		rigidbodyComponent.velocity = movement;
+	}
+
+	void OnCollisionEnter2D(Collision2D collisionInfo)
+	{
+		if (collisionInfo.collider.tag == cameraTag)
+		{
+			GetComponent<BoxCollider2D>().enabled = false;
+			StartCoroutine(EnableBox(1.0f));
+		}
+
+		if (collisionInfo.collider.tag == playerTag)
+		{
+			rigidbodyComponent.freezeRotation = false;
+			hit = true;
+		}
+	}
+
+	IEnumerator EnableBox(float waitTime)
+	{
+		yield return new WaitForSeconds(waitTime);
+		GetComponent<BoxCollider2D>().enabled = true;
+	}
+}
