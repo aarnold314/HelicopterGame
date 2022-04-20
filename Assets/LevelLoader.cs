a43f784fd6807d7397b4a689bf23dca034a3948f
aarnold314 <103523562+aarnold314@users.noreply.github.com>
2022-04-20 18:07:48 -0400

    make loader use names rather than indexes

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -5,30 +5,29 @@ using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
-    public GameObject loadingScreen;
-    public GameObject previousScreen;
-    public Slider slider;
+	public GameObject loadingScreen;
+	public GameObject previousScreen;
+	public Slider slider;
 
-    public void LoadLevel (int sceneIndex)
-    {
-        StartCoroutine(LoadAsyncgronously(sceneIndex));
-        Debug.Log("In levelloader");
-    }
+	public void LoadLevel(string sceneName)
+	{
+		StartCoroutine(LoadAsynchronously(sceneName));
+		Debug.Log("In levelloader");
+	}
 
-    IEnumerator LoadAsyncgronously (int sceneIndex)
-    {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+	private IEnumerator LoadAsynchronously(string sceneName)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
 
-        previousScreen.SetActive(false);
-        loadingScreen.SetActive(true);
+		previousScreen.SetActive(false);
+		loadingScreen.SetActive(true);
 
-        while (!operation.isDone)
-        {
-            float progress = Mathf.Clamp01(operation.progress / 0.9f);
-            slider.value = progress;
+		while (!operation.isDone)
+		{
+			float progress = Mathf.Clamp01(operation.progress / 0.9f);
+			slider.value = progress;
 
-            yield return null;
-        }
-
-    }
+			yield return null;
+		}
+	}
 }
