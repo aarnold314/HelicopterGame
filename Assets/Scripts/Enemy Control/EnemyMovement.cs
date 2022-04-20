6f6656253bce1e69c68b5fe2d9a3a3ea46914ef7
aarnold314 <103523562+aarnold314@users.noreply.github.com>
2022-04-19 21:51:01 -0400

    move enemy movement to FixedUpdate

--- Assets/Scripts/Enemy Control/EnemyMovement.cs
+++ Assets/Scripts/Enemy Control/EnemyMovement.cs
@@ -1,70 +1,59 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+	[SerializeField] private float frequency = 5f;
+	[SerializeField] private float magnitude = 5f;
 
-    [SerializeField]
-    private float frequency = 5f;
+	public Vector2 speed = new Vector2(10, 10);
+	public PlayerMovement playerMovement;
+	public bool hit = false;
 
-    [SerializeField] 
-    private float magnitude = 5f;
+	private Rigidbody2D rigidbodyComponent;
 
-    public Vector2 speed = new Vector2(10, 10);
-    public Vector2 direction;
-    public PlayerMovement playerMovement;
-    public bool hit = false;
+	private void Awake()
+	{
+		rigidbodyComponent = GetComponent<Rigidbody2D>();
+	}
 
-    private Vector2 movement;
-    private Vector2 startPosition;
+	private void FixedUpdate()
+	{
+		var direction = new Vector2(-1, Mathf.Sin(Time.time * frequency) * magnitude);
+		Vector2 movement;
+		if (hit)
+		{
+			movement = new Vector2(0, 0);
+		}
+		else
+		{
+			movement = new Vector2(
+				speed.x * direction.x,
+				speed.y * direction.y
+			);
+		}
 
-    private Rigidbody2D rigidbodyComponent;
+		// Apply movement to the rigidbody
+		rigidbodyComponent.velocity = movement;
+	}
 
+	void OnCollisionEnter2D(Collision2D collisionInfo)
+	{
+		if (collisionInfo.collider.tag == "MainCamera")
+		{
+			GetComponent<BoxCollider2D>().enabled = false;
+			StartCoroutine(EnableBox(1.0f));
+		}
 
-    void Start()
-    {
-        startPosition = transform.position;
-    }
-    void Update()
-    {
-        direction = new Vector2(-1, Mathf.Sin(Time.time * frequency) * magnitude);
-       if (hit)
-       {
-           movement = new Vector2(0,0);
-       } else{
-           movement = new Vector2(
-          speed.x * direction.x,
-          speed.y * direction.y);
-       }
-        
-        //transform.position = startPosition + transform.up * Mathf.Sin(Time.time * frequency + offset) * magnitude;
-    }
+		if (collisionInfo.collider.tag == "Player")
+		{
+			hit = true;
+		}
+	}
 
-    void FixedUpdate()
-    {
-        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
-
-        // Apply movement to the rigidbody
-        rigidbodyComponent.velocity = movement;
-    }
-
-    void OnCollisionEnter2D (Collision2D collisionInfo)
-    {
-        if (collisionInfo.collider.tag == "MainCamera")
-        {
-           GetComponent<BoxCollider2D>().enabled = false;
-           StartCoroutine(EnableBox(1.0f));
-        }
-        if (collisionInfo.collider.tag == "Player")
-        {
-            hit = true;
-        }
-    }
-
-    IEnumerator EnableBox(float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);
-        GetComponent<BoxCollider2D>().enabled = true;
-    }
+	IEnumerator EnableBox(float waitTime)
+	{
+		yield return new WaitForSeconds(waitTime);
+		GetComponent<BoxCollider2D>().enabled = true;
+	}
 }
