3bc359146fc286780b0fe0609dcfecdcbe87b01d
anniewormus <annie@wormus.com>
2022-04-19 23:32:19 -0400

    added explosion effect upon collision

---
+++ Assets/Explosion.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    // public GameObject explosionEffect;
+    // public GameObject deadObject;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     GameObject explosionEffectIns = Instantiate(explosionEffect,transform.position,Queternion.identity);
+    //     Destroy(explosionEffectIns,10);
+    // }
+}
--- Assets/Scripts/Player Control/PlayerCollision.cs
+++ Assets/Scripts/Player Control/PlayerCollision.cs
@@ -4,16 +4,31 @@ public class PlayerCollision : MonoBehaviour
 {
     // creates reference to player movement script 
     public PlayerMovement movement;
+    public GameObject explosionEffect;
+    
 
     void OnCollisionEnter2D (Collision2D collisionInfo)
     {
         if (collisionInfo.collider.tag == "Enemy")
         {
+
+            float inputX = Input.GetAxis("Horizontal");
+		    float inputY = Input.GetAxis("Vertical");
             
             // currently disables player movement when it collides with an enemy
             // change to an explosion graphic
            movement.enabled = false;
+           
+        //    explosionEffect.SetActive(true);
+        //    explosionEffect.transform.position = new Vector2(inputX,inputY);
+            GameObject explosionEffectIns = Instantiate(explosionEffect,transform.position,Quaternion.identity);
+            explosionEffectIns.SetActive(true);
+            explosionEffectIns.transform.localScale = new Vector2(10f,10f);
+            explosionEffectIns.transform.Translate(0,70f,0);
+            Destroy(explosionEffectIns,3);
+
            FindObjectOfType<GameManager>().GameOver();
+
         }
     }
 }
