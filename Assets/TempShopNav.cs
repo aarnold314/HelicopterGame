c0f8f080035260e4b1bb71124f90720fd48fb6a7
anniewormus <annie@wormus.com>
2022-04-21 12:52:40 -0400

    created environments for 3 levels

--- Assets/TempShopNav.cs
+++ Assets/TempShopNav.cs
@@ -3,8 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class TempShopNav : MonoBehaviour
 {
+    [SerializeField] public string sceneName;
+
+    // void Start()
+    // {
+    //     // scene = SceneManager.GetActiveScene();
+    //     // Debug.Log("Active Scene name is: " + scene.name + "\nActive Scene index: " + scene.buildIndex);
+    // }
+
     public void NextScene ()
     {
-        SceneManager.LoadScene("Objective2");
+        SceneManager.LoadScene(sceneName);
     }
 }
