d169bfcc9de52e17d9d82785232c1a01c290be9e
anniewormus <annie@wormus.com>
2022-04-22 08:23:01 -0400

    health bar updates

--- Assets/Scripts/Scene Control/Score.cs
+++ Assets/Scripts/Scene Control/Score.cs
@@ -7,6 +7,10 @@ public class Score : MonoBehaviour
     // reference to player
     public Transform player;
     public Text scoreText;
+    // public Slider slider;
+
+    // public Health health;
+    // public GameObject heli;
 
     // Update is called once per frame
     void Update()
@@ -16,5 +20,10 @@ public class Score : MonoBehaviour
         {
         scoreText.text = player.position.x.ToString("0");
         }
+
+        // Trying to grab health of player
+        // health = heli.GetComponent<Health>();
+        // Debug.Log(health);
+        // slider.value = health.CurrentHealth;
     }
 }
